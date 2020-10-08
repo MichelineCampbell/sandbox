d5600c0ce2cf02304066036312e06f4a8230fdab
MichelineCampbell <michelineleecampbell@gmail.com>
2020-10-08 17:22:21 +1000

    learning netcdf

--- netcdf_learning/correlating_netcdf.R
+++ netcdf_learning/correlating_netcdf.R
@@ -7,6 +7,7 @@
 ## Contact info: michelineleecampbell@gmail.com
 ## Notes: 
 
+### Any missing values in a cell will fuck up correlations/lm/mean/whatever (result in NA), so consider sub-settng. Only SEA has coverage for the whole period of GISTEMP
 
 # packages ----------------------------------------------------------------
 
@@ -18,13 +19,17 @@ library(rgdal) # package for geospatial analysis
 library(ggplot2)
 library(rasterVis)
 library(pals)
+library(janitor)
 
 source("H:/General_R/update_theme.R")
 
 
 # data --------------------------------------------------------------------
-
+sydtemp <- read.csv("netcdf_learning/sydney_temp/IDCJAC0002_066062_Data1.csv")
 nc_data <- nc_open("D:/CampbellM1/R_learning_space/gisstemp_2020/gistemp250_GHCNv4.nc")
+
+sydtemp <- clean_names(sydtemp)
+
 
 
 lon <- ncvar_get(nc_data, "lon")
@@ -50,7 +55,10 @@ plot(cropped_gistemp[[1688]]) # check
 fakedata <- data.frame(t = seq(1:1688), fakey = rnorm(1688, mean = 20, sd = 1)* t^(0.2))
 
 fun <- function(x) {
-  cor(x, fakedata$fakey)
+  cor(x, sydtemp$mean_maximum_temperature_a_c)
+}
+fun <- function(x) {
+  mean(x, na.rm = TRUE)
 }
 x2 <- raster::calc(cropped_gistemp, fun)
 
@@ -64,15 +72,15 @@ x2cor <- extract(x2, matrix(c(145, -30), ncol =2))
 x2cor
 
 
-tempCuts <- seq(-4, 4, length.out = 17)
+
 aus <- ne_countries(country = "australia")
 rasterVis::gplot(x2) + 
-  geom_tile(aes(fill = cut(value, tempCuts))) +
+  geom_tile(aes(fill = value)) +
   geom_path(data = aus, aes(x = long, y = lat, group = group)) +
   geom_point(aes(x = 145, y = -30)) +
-  scale_fill_brewer(palette = "RdBu",
-                    drop = FALSE,
-                    direction = -1) +
+  # scale_fill_brewer(palette = "RdBu",
+                    # drop = FALSE,
+                    # direction = -1) +
   coord_equal() +
   theme_MC_grid() +
   labs(x = "Longitude",
@@ -80,7 +88,7 @@ rasterVis::gplot(x2) +
        fill = "Temperature anomaly \nrelative to 1951-1980",
        title = "August 2020 GISTEMP V4 \nSurface Air Temperature Anomaly",
        subtitle = "Surface Air Temperature (no ocean data): 250 km smoothing") +
-  guides(fill = guide_legend(override.aes = list(size = 2),ncol = 2)) +
+  # guides(fill = guide_legend(override.aes = list(size = 2),ncol = 2)) +
   theme(plot.title = element_text(size = 8),
         plot.subtitle = element_text(size = 6),
         legend.title = element_text(size = 6),
